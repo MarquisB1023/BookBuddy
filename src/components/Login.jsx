549ab400261be6e987acd10d81f5c34b2bbe9ce3
marquis brunbdage <marquisbrunbdage@marquiss-MacBook-Pro.local>
2024-03-15 20:30:33 -0400

    react npm issue

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,64 +1,69 @@
-import React, { useState } from "react";
-// import { login } from "./API";
-import { Link } from "react-router-dom";
+/* TODO - add your code to create a functional React component that renders a login form */
+
+import {useState} from 'react'
+// import {link } from 'react-router-dom'
+
 
 const [Email, setEmail] = useState("");
 const [Password, setPassword] = useState("");
-const [error, setError] = useState("");
+const [errpr, setError] = useState(null);
 
-function Login({ token, setFunction }) {
-  async function handleSubmit(event) {
-    event.preventDefault();
-    try {
-      const response = await fetch(
-        `https://fsa-book-buddy-b6e748d1380d.herokuapp.com//api/users/login`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringyify({
-            email: "ssmith@xample.com",
-            password: "sam345",
-            Authorization: `Bearer ${token}`,
-          }),
-        }
-      );
-      const result = await response.json();
-      console.log(result);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+function Login ({token , setFunction }) {
 
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+     try {
+        const resposne = await fetch(
+            `https://fsa-book-buddy-b6e748d1380d.herokuapp.com//api/users/login`,{
+            method : "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringyify({
+                email: 'ssmith@xample.com',
+                password: 'sam345'
+            })
+        });
+        const result = await Response.json();
+        console.log(result);
+
+     }catch(error) {
+        console.error(error);
+     }
+}
+
+     return (
+   <>
+    <form onSubmit={handeSubmi}>
         <h2>Email</h2>
-        {error && <p>{error}</p>}
+        {error && <p>{errpr}</p>}
         <label>
-          Email:
-          <input
+            Email:
+            <input 
             email="Email"
             value={Email}
             onChange={(e) => setEmail(e.target.value)}
-          />
+            />
         </label>
 
         <h2>Password</h2>
-        {error && <p>{error}</p>}
+        {error && <p>{errpr}</p>}
         <label>
-          Password:{""}
-          <input
-            passwordl="Password"
+            Password:{""}
+            <input 
+            email="Password"
             value={Password}
             onChange={(e) => setPassword(e.target.value)}
-          />
+            />
         </label>
         <button type="submit">Login</button>
-      </form>
-    </>
-  );
-}
+    </form>
+   </>
+     )
 
-export default Login;
+
+    
+};
+
+export default Login
